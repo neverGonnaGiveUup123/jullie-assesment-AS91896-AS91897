bc481e8d8642a546f70d92f48e875e1929f7d641
Siddharth <st22041@ormiston.school.nz>
2023-05-15 11:26:46 +1200

    Added delete button functionality

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import ttkbootstrap as ttkb
+import json
 from ttkbootstrap.constants import *
 from title import Heading
 from entries import Entries
@@ -8,16 +9,19 @@ class julieGui(ttkb.Window):
     def __init__(self, title="ttkbootstrap", themename="litera", iconphoto='', size=None, position=None, minsize=None, maxsize=None, resizable=None, hdpi=True, scaling=None, transient=None, overrideredirect=False, alpha=1):
         super().__init__(title, themename, iconphoto, size, position, minsize, maxsize, resizable, hdpi, scaling, transient, overrideredirect, alpha)
 
+        with open('src/stored_data.json', 'w') as file:
+            json.dump({"Customer name": [], "Receipt": [], "Item hired": [], "Hired item amount": []},file)
+
         self.title('AS91896 & AS91897')
 
         self.heading = Heading(self)
-        self.heading.grid(row=0,column=0,columnspan=2,padx=20,pady=10)
+        self.heading.grid(row=0,column=0,padx=20,pady=10)
 
         self.entries = Entries(self)
-        self.entries.grid(row=1,column=0,columnspan=2,padx=20,pady=10)
+        self.entries.grid(row=1,column=0,padx=20,pady=10)
 
         self.select_data = SelectData(self)
-        self.select_data.grid(row=2,column=0,padx=10,pady=10,columnspan=2)
+        self.select_data.grid(row=0,column=1,padx=10,pady=10)
 
 if __name__ == '__main__':
     app = julieGui()
--- src/select_data.py
+++ src/select_data.py
@@ -11,12 +11,21 @@ class SelectData(ttkb.Frame):
         def refresh_func():
             with open('src/stored_data.json', 'r') as file:
                 self.customer_names_list = json.load(file)['Customer name']
+            self.display_checkbox.config(values=self.customer_names_list)
         
         def select_data_func():
             self.display_checkbox.config(values=self.customer_names_list)
 
         def delete_button_func():
-            pass
+            with open('src/stored_data.json', 'r') as file:
+                data = json.load(file)
+                selected_data = self.display_checkbox.get()
+                index = data["Customer name"].index(selected_data)
+                for keys in data.keys():
+                    data[keys].pop(index)
+            with open('src/stored_data.json', 'w') as file:
+                json.dump(data,file)
+
 
         def display_button_func():
             pass
@@ -25,8 +34,11 @@ class SelectData(ttkb.Frame):
         self.display_checkbox.config(values=tuple(self.customer_names_list))
         self.display_checkbox.grid(row=0,column=0,columnspan=2)
         
-        self.update_button = ttkb.Button(self,text="Refresh names", command=refresh_func, bootstyle=INFO)
+        self.update_button = ttkb.Button(self,text="Refresh data", command=refresh_func, bootstyle=INFO)
         self.update_button.grid(row=1,column=0,columnspan=2,pady=10)
 
-        self.delete_button = ttkb.Button(self,command=delete_button_func,bootstyle=DANGER)
+        self.delete_button = ttkb.Button(self,command=delete_button_func,bootstyle=DANGER, text="Delete selected data")
         self.delete_button.grid(row=3,column=0,padx=10,pady=10)
+
+        self.display_button = ttkb.Button(self,command=display_button_func,bootstyle=SUCCESS, text="Display selected data")
+        self.display_button.grid(row=3,column=1,padx=10,pady=10)
